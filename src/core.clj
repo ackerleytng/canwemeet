20f97138875f7760aea6c6fc97319add82d2d1a1
Ackerley Tng <ackerleytng@gmail.com>
2020-05-01 11:44:13 +0800

    Add meeting time recommendation feature

--- src/core.clj
+++ src/core.clj
@@ -63,6 +63,44 @@
    :working [10 17]
    :evening [17 23]})
 
+(defn preferences
+  "Hour-of-day for calls, in order of preference:
+
+  early in the evening [17 - 21)
+  as late as possible in the morning hours [8 - 10)
+  night [21 - 24)
+  as late as possible early morning [5 - 8)
+  lunchtime (12)
+  lunchtime (13)
+  earliest overlapping hour after lunch (14 - 17)
+  latest overlapping hour before lunch (10 - 12)"
+  []
+  (concat
+   (range 17 21)
+   (reverse (range 8 10))
+   (range 21 24)
+   (reverse (range 5 8))
+   [12 13]
+   (range 14 17)
+   (reverse (range 10 12))))
+
+(defn within-range
+  "Whether t is l <= t, t + hours < h (assumes that l is before h)"
+  [[l h] hours t]
+  (and (not (t/after? l t))
+       (t/before? (t/minus
+                   (t/plus t (t/hours hours))
+                   (t/minutes 1))
+                  h)))
+
+(defn recommend-time-within-range
+  "Given a time range in a time zone, recommend a meeting time in the same time
+  zone. Will return a recommended time according to (preferences)."
+  [range]
+  (first
+   (filter (partial within-range range 1)
+           (map (partial forward-to-next (first range)) (preferences)))))
+
 (def range-wordings
   {:morning "Their mornings are"
    :working "They are at work"
@@ -82,13 +120,48 @@
                  (s/lower-case low) "and"
                  (s/lower-case high) "your time"])))
 
+(defn recommend-meeting-time
+  "Given user time and target time now, recommend a meeting time in the user time
+  zone, for a meeting of duration hours. hours defaults to 1"
+  ([user-time-now their-time-now]
+   (recommend-meeting-time user-time-now their-time-now 1))
+  ([user-time-now their-time-now hours]
+   (let [their-working-range-user-tz (map #(t/with-zone-same-instant
+                                             (forward-to-next their-time-now %) user-time-now)
+                                          (:working ranges))
+         recommended-time-user-tz (recommend-time-within-range their-working-range-user-tz)]
+     recommended-time-user-tz)))
+
+(defn format-recommended-meeting-time
+  ([user-time-now their-time-now]
+   (format-recommended-meeting-time user-time-now their-time-now 1))
+  ([user-time-now their-time-now hours]
+   (let [recommended-time-format "h a, EE d MMMM"
+         recommended-time-user-tz (recommend-meeting-time user-time-now their-time-now hours)
+         recommended-time-their-tz (when recommended-time-user-tz
+                                     (t/with-zone-same-instant
+                                       recommended-time-user-tz
+                                       their-time-now))]
+     (if recommended-time-user-tz
+       [(str "Tell them you want to meet at "
+             (t/format recommended-time-format recommended-time-their-tz)
+             ", their time")
+        (str "That's "
+             (t/format recommended-time-format recommended-time-user-tz)
+             ", your time")]
+       ["Can't find a good time to meet!"]))))
+
 (defn format-output-lines
   [user-time-now {:keys [formatted time-zone dst-observed time-zone-id]}]
   (let [their-time-now (t/with-zone-same-instant user-time-now time-zone-id)
-        header (str (s/join " " [formatted "is in the" time-zone "time zone"])
-                    (if dst-observed " (DST is observed now)"))
+        recs (format-recommended-meeting-time user-time-now their-time-now)
+        meta (str (s/join " " [formatted "is in the" time-zone "time zone"])
+                  (if dst-observed " (DST is observed now)"))
         times (map (partial format-range user-time-now their-time-now) wording-times)]
-    (concat [header] times)))
+    (concat
+     recs
+     [meta]
+     times)))
 
 (defn meet-info [user-time-now address]
   (let [location (geocode address)
@@ -113,4 +186,6 @@
       (usage)
       (meet-info (user-time) (first args))))))
 
-(-main ["austin"])
+(comment
+  (-main ["san francisco"])
+  )
